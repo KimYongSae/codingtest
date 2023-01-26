758f4ffd37cf2aa14a33a2426ae378703bb0703c
kim-yongsae <kim-yongsae@218.38.137.28>
2023-01-26 12:05:09 +0900

    ll

--- myapp/src/main/java/org/zerock/myapp/baekjoon/sliver/스택.java
+++ myapp/src/main/java/org/zerock/myapp/baekjoon/sliver/스택.java
@@ -1,9 +1,30 @@
 package org.zerock.myapp.baekjoon.sliver;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Ω∫≈√ {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int line = Integer.parseInt(br.readLine());
+		
+		int answer[] = new int[line];
+		String arr[] = new String[line];
+		
+		for(int i = 0; i < line; i ++) {
+			arr[i] = br.readLine();
+		}
+		
+		
+		
+		
+		
+		
+	} // main
 
-	}
-
-}
+	
+} // end class
