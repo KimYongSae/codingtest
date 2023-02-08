4942775baf397116077732af755324c4aa51b71e
KimYongSae <yonggale3@naver.com>
2023-02-08 15:11:20 +0900

    a

---
+++ myapp/src/main/java/org/zerock/myapp/baekjoon/gold/tmp.java
@@ -0,0 +1,17 @@
+package org.zerock.myapp.baekjoon.gold;
+
+public class tmp {
+
+	public static void main(String args[]) {
+		
+		int a = 0;
+		int b = 0;
+		
+		for(int i = 0; i < 10; i ++) {
+			a  =+ 1;
+			b += 1;
+			System.out.println(a);
+			System.out.println(b);
+		}
+	}
+}
---
+++ myapp/src/main/java/org/zerock/myapp/programmers/lv2/가장_큰_수.java
@@ -0,0 +1,11 @@
+package org.zerock.myapp.programmers.lv2;
+
+public class 가장_큰_수 {
+
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
+ 
