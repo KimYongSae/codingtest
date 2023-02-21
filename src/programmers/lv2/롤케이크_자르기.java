1e6d571be3136716e602abdfd7b743642a408686
KimYongSae <yonggale3@naver.com>
2023-02-21 10:45:40 +0900

    인텔리제이

--- src/programmers/lv2/롤케이크_자르기.java
+++ src/programmers/lv2/롤케이크_자르기.java
@@ -1,14 +1,37 @@
 package programmers.lv2;
 
+import java.util.Arrays;
 import java.util.HashSet;
 
 public class 롤케이크_자르기 {
 
     public static int Solution(int[] topping){
-        HashSet<Integer> hs1 = new HashSet<>();
-        HashSet<Integer> hs2 = new HashSet<>();
+        HashSet<Integer> hs = new HashSet<>();
+
+        int[] tmp1 = new int[topping.length];
+        int[] tmp2 = new int[topping.length];
+
+        for (int i = 0; i < topping.length; i++) {
+            hs.add(topping[i]);
+            tmp1[i] = hs.size();
+
+        }
+        hs.clear();
+        for (int i = topping.length-1; i >= 0; i--) {
+            hs.add(topping[i]);
+            tmp2[i] = hs.size();
+
+        }
 
         int answer = 0;
+
+        for (int i = 0; i < topping.length-1; i++) {
+            if (tmp1[i] == tmp2[i+1]) {
+                answer ++;
+            }
+        }
+
+
 
 
     return answer;
