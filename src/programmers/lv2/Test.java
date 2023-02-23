1c6990d86c60597a3121f893ba7aae6b7c5ca689
KimYongSae <yonggale3@naver.com>
2023-02-23 13:01:41 +0900

    ad

--- src/programmers/lv2/Test.java
+++ src/programmers/lv2/Test.java
@@ -1,10 +1,14 @@
-package org.zerock.myapp.programmers.lv2;
+package programmers.lv2;
+
+import java.util.Arrays;
 
 public class Test {
 	public static void main(String args[]) {
 
+		String abc = "http://localhost:8080/*";
 
-		System.out.println("abc");
+		String[] tmp = abc.split("/");
+		System.out.println(tmp[tmp.length-1]);
 	}
 }
 	
