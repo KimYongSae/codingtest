76c9c4533a75bc09f03c48879bdeadc0d907cef6
KimYongSae <yonggale3@naver.com>
2023-10-31 17:55:17 +0900

    10/31-2

--- src/programmers/lv2/숫자의_표현.java
+++ src/programmers/lv2/숫자의_표현.java
@@ -5,13 +5,22 @@ public class 숫자의_표현 {
     public static int solution(int n){
         int answer = 0;
 
-        for (int i = 0; i < n/2; i++) {
+        for (int i = 1; i <= n/2; i++) {
+        int temp = i;
 
+            for (int j = i+1; j < n; j++) {
 
+                temp += j;
+
+                if(n == temp){
+                    answer ++;
+                    break;
+                } else if(temp >n){
+                    break;
+                }
+            }
         }
-        
-
-        return answer;
+        return answer+1;
     }
     public static void main(String[] args) {
 
